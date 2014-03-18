eb6ec7e35c0224b68dbc5fc637f75a59446a315b
JamesHealey94 <jameshealey1994@gmail.com>
2014-03-18 23:01:38 +0000

    Initial netbeans commit

---
+++ SimpleTowns-Dynmap/src/com/gmail/jameshealey1994/simpletownsdynmap/SimpleTownsDynmap.java
@@ -0,0 +1,17 @@
+package com.gmail.jameshealey1994.simpletownsdynmap;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Dynmap integration for the SimpleTowns Bukkit plugin.
+ *
+ * @author JamesHealey94 <jameshealey1994.gmail.com>
+ */
+public class SimpleTownsDynmap extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        //
+    }
+}
